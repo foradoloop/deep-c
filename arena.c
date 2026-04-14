8a75b97bdc4a70110e0790ae961b955e41656c69
foradoloop <255560900+foradoloop@users.noreply.github.com>
2026-04-14 20:54:19 -0300

    Implement arena allocator

---
+++ arena.c
@@ -0,0 +1,64 @@
+#include "arena.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void arena_create(Arena *a, size_t size)
+{
+	a->ptr = malloc(size);
+	if (!a->ptr) {
+		fprintf(stderr, "Arena malloc fail\n");
+		exit(1);
+	}
+	a->size = size;
+	a->offset = 0;
+	a->checkpoint = 0;
+}
+
+void arena_init(Arena *a, unsigned char *buffer, size_t buflen)
+{
+	a->ptr = buffer;
+	a->size = buflen;
+	a->offset = 0;
+	a->checkpoint = 0;
+}
+
+void *arena_alloc(Arena *a, size_t alloc)
+{
+	if (a->offset + alloc > a->size) {
+		fprintf(stderr, "Arena overflow\n");
+		exit(1);
+	}
+
+	void *ptr;
+
+	ptr = a->ptr + a->offset;
+	a->offset += alloc;
+
+	return ptr;
+}
+
+void arena_checkpoint(Arena *a)
+{
+	a->checkpoint = a->offset;
+}
+
+void arena_restore(Arena *a)
+{
+	a->offset = a->checkpoint;
+}
+
+void arena_reset(Arena *a)
+{
+	a->offset = 0;
+	a->checkpoint = 0;
+}
+
+void arena_destroy(Arena *a)
+{
+	free(a->ptr);
+	a->ptr = NULL;
+	a->size = 0;
+	a->offset = 0;
+	a->checkpoint = 0;
+}
+
---
+++ arena.h
@@ -0,0 +1,34 @@
+#ifndef ARENA_H
+#define ARENA_H
+
+#include <stddef.h>
+
+#define arena_alloc_obj(a, type) \
+	arena_alloc(a, sizeof(type))
+
+#define arena_alloc_arr(a, type, count) \
+	arena_alloc(a, sizeof(type) * (count))
+
+#define  B (sizeof(unsigned char))
+#define KB (1024 * B)
+#define MB (1024 * KB)
+#define GB (1024 * MB)
+
+struct arena {
+	unsigned char *ptr;
+	size_t size;
+	size_t offset;
+	size_t checkpoint;
+};
+typedef struct arena Arena;
+
+void arena_create(Arena *a, size_t size);
+void arena_init(Arena *a, unsigned char *buffer, size_t buflen);
+void *arena_alloc(Arena *a, size_t alloc);
+void arena_checkpoint(Arena *a);
+void arena_restore(Arena *a);
+void arena_reset(Arena *a);
+void arena_destroy(Arena *a);
+
+#endif
+
